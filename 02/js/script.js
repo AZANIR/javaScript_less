c1728858e2613a34fa8625893fa36c88aec2613a
AZANIR <eleoneks@gmail.com>
2020-09-21 20:26:35 +0300

    update

--- 02/js/script.js
+++ 02/js/script.js
@@ -1,1 +1,8 @@
-console.log(1);
+//"use strict";
+
+//alert("Hello!");
+//const result = confirm("A you here?");
+//console.log(result);
+
+const answer = prompt("A you 18?", "18");
+console.log(typeof (answer));
