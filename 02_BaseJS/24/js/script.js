5de3ccea1dd7b9ab320e450ae629cf17b9bfbca7
AZANIR <eleoneks@gmail.com>
2021-06-24 16:03:35 +0300

    update HW

--- 02_BaseJS/24/js/script.js
+++ 02_BaseJS/24/js/script.js
@@ -1,85 +1,36 @@
-/* Задание на урок:
-
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
-
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
-
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
-
 "use strict";
 
+//	Какое будет выведено значение: let x = 5; alert( x++ ); ?
+// alert show 5
 
-let numberOfFilms;
+//	Чему равно такое выражение: [ ] + false - null + true ?
+//NaN
 
+//	Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+// 2
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies:{},
-  actors:{},
-  genres:[],
-  privat:false,
-  start:function(){
-    personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
-    while(personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
-        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
-        }
-    },
-    rememberMyFilms: function(){
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('Один из последних просмотренных фильмов?',''),
-              b = prompt('На сколько оцените его?','');
-            
-            if(a != null && b != null && a != '' && b != '' && a.length <50){
-                personalMovieDB.movies[a] = b;
-                console.log("All right");
-            }else{
-                console.log("error");
-                i--;
-            }    
-        }
-    },
-    detectPersonalLevel: function(){
-        if(personalMovieDB.count<10){
-            console.log("Просмотрено довольно мало фильмов");
-        }else if(personalMovieDB.count>=10 && personalMovieDB.count<30){
-            console.log("Вы классический зритель");
-        }else if(personalMovieDB.count>=30){
-            console.log("Вы киноман");
-        }else{
-            console.log("Произошла ошибка");
-        }
-    },
-    showMyDB: function(hidden){
-        if(!hidden){
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB:function(){
-        if(personalMovieDB.privat){
-            personalMovieDB.privat = false;
-        }else{
-            personalMovieDB.privat = true;
-        }
-    },
-    writeYourGenres(){
-        for(let i=1;i <=3; i++){
-            let genre = prompt(`Ваш любимый фильм под номером ${i}`);
-            if(genre === '' || genre == null){
-                console.log('Вы ввели некорректные данные или не ввели их совсем');
-                i--;
-            }else{
-                personalMovieDB.genres[i-1] = genre;
-            }
-        }
+//	Чему равна сумма [ ] + 1 + 2?
+//12 (String)
 
-        personalMovieDB.genres.forEach((item, i)=>{
-            console.log(`Любимый жанр ${i+1} - это ${item}`);
-        });
-    }
-};
+//	Что выведет этот код: alert( "1"[0] )?
+//1(String)
+
+//	Чему равно 2 && 1 && null && 0 && undefined ?
+//null
+//	Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+//false
+
+//	Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
+//3
+
+//	a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+//false
+
+//	Что выведет этот код: alert( +"Infinity" ); ?
+//Infinity(Number)
+
+//	Верно ли сравнение: "Ёжик" > "яблоко"?
+//false
+
+//	Чему равно 0 || "" || 2 || undefined || true || falsе ?
+//2
